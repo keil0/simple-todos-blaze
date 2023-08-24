8cc7dd8e52cb50a92eea05711a3b06470653605b
Pierre Cruz <contact@pierrecruz.com>
2023-08-24 16:47:59 +0200

    Add user management : - Restrict tasks list to the connected user - Login logic & form - Logout logic & button

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -4,14 +4,68 @@ import { TasksCollection } from "../api/TasksCollection";
 
 import "./App.html";
 import "./Task.js";
+import "./Login.js";
+
+// User session
+const getUser = () => Meteor.user();
+const isUserLogged = () => !!getUser();
+
+// Task status
+const HIDE_COMPLETED_STRING = "hideCompleted";
+
+const getTasksFilter = () => {
+  const user = getUser();
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+  const userFilter = user ? { userId: user._id } : {};
+  const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
+
+  return { userFilter, pendingOnlyFilter };
+};
 
 Template.mainContainer.onCreated(function mainContainerOnCreated() {
   this.state = new ReactiveDict();
 });
 
+Template.mainContainer.events({
+  "click #hide-completed-button"(event, instance) {
+    const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+    instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
+  },
+  "click .user"() {
+    Meteor.logout();
+  },
+});
+
 Template.mainContainer.helpers({
   tasks() {
-    return TasksCollection.find({}, { sort: { createdAt: -1 } });
+    const instance = Template.instance();
+    const hideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+    const { pendingOnlyFilter, userFilter } = getTasksFilter();
+
+    if (!isUserLogged()) {
+      return [];
+    }
+
+    return TasksCollection.find(hideCompleted ? pendingOnlyFilter : userFilter, {
+      sort: { createdAt: -1 },
+    }).fetch();
+  },
+  hideCompleted() {
+    return Template.instance().state.get(HIDE_COMPLETED_STRING);
+  },
+  incompleteCount() {
+    if (!isUserLogged()) {
+      return "";
+    }
+    const { pendingOnlyFilter } = getTasksFilter();
+    const incompleteTasksCount = TasksCollection.find(pendingOnlyFilter).count();
+    return incompleteTasksCount ? `(${incompleteTasksCount})` : "";
+  },
+  isUserLogged() {
+    return isUserLogged();
+  },
+  getUser() {
+    return getUser();
   },
 });
 
@@ -27,6 +81,7 @@ Template.form.events({
     // Insert a task into the collection
     TasksCollection.insert({
       text,
+      userId: getUser()._id,
       createdAt: new Date(), // current time
     });
 
